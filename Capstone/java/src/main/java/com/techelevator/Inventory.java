e169cb8023fa3bfd3103438f02e3db5724ee2c63
Anne Hoback <ahoback76@gmail.com>
2021-02-04 14:51:38 -0500

    item done

--- Capstone/java/src/main/java/com/techelevator/Inventory.java
+++ Capstone/java/src/main/java/com/techelevator/Inventory.java
@@ -1,5 +1,29 @@
 package com.techelevator;
 
-public class Inventory {
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Map;
 
+public class Inventory {//create the class
+	private String invName;
+	private Map <String[], Integer> machineInv; //will have a string array from the data file as key, the value is the quantity in stock
+	private static final int DEFAULT_STARTING_STOCK = 5;
+	private int currentStock;
+	
+	
+	//create constructors
+		//default constructor
+	public Inventory () {
+		invName = null;
+		machineInv = null;
+	}
+	
+	public Inventory (String invName, String dataFileName) throws FileNotFoundException {
+
+	
+	
+		this.leagueName = leagueName; //.this required because the paramater has the same name as data name
+		//load the data from the file given as an arguement into our leagueMembers
+		//load the data from the file given into leagueMembers using member function
+		loadFile(dataFileName);// run the loadFile method sending the file to be loaded
 }
--- Capstone/java/src/main/java/com/techelevator/Item.java
+++ Capstone/java/src/main/java/com/techelevator/Item.java
@@ -1,5 +1,107 @@
 package com.techelevator;
 
-public class Item {
+public class Item { //create Class
+	String slot;
+	String name;
+	int price;
+	String type;
 
-}
+		
+	public Item (String slot, String name, int price, String type){//constructor with all parameters
+		this.slot = slot;
+		this.name=name;
+		this.price=price;
+		this.type=type;
+	}
+	/**
+	 * @return the slot
+	 */
+	public String getSlot() {
+		return slot;
+	}
+	/**
+	 * @param slot the slot to set
+	 */
+	public void setSlot(String slot) {
+		this.slot = slot;
+	}
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	/**
+	 * @return the price
+	 */
+	public int getPrice() {
+		return price;
+	}
+	/**
+	 * @param price the price to set
+	 */
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	/**
+	 * @return the type
+	 */
+	public String getType() {
+		return type;
+	}
+	/**
+	 * @param type the type to set
+	 */
+	public void setType(String type) {
+		this.type = type;
+		}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + price;
+		result = prime * result + ((slot == null) ? 0 : slot.hashCode());
+		result = prime * result + ((type == null) ? 0 : type.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Item other = (Item) obj;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (price != other.price)
+			return false;
+		if (slot == null) {
+			if (other.slot != null)
+				return false;
+		} else if (!slot.equals(other.slot))
+			return false;
+		if (type == null) {
+			if (other.type != null)
+				return false;
+		} else if (!type.equals(other.type))
+			return false;
+		return true;
+			}
+	@Override
+	public String toString() {
+		return "Item [slot=" + slot + ", name=" + name + ", price=" + price + ", type=" + type + "]";
+	}
+	
+	}//end of class body
